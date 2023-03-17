d128d661f936527e1589aa810d5f7d08947a02ff
Rolly Maulana Awangga <rolly@awang.ga>
2023-03-18 06:31:45 +0700

    ok

--- report.go
+++ report.go
@@ -2,8 +2,6 @@ package presensi
 
 import (
 	"encoding/csv"
-	"fmt"
-	"log"
 	"os"
 	"time"
 
@@ -17,24 +15,28 @@ func GenerateReportCurrentMonth(MongoConn *mongo.Database, to types.JID, whatsap
 	location, _ := time.LoadLocation("Asia/Jakarta")
 	atmessage.SendMessage("Mohon tunggu sebentar, laporan sedang di buat ya kak", to, whatsapp)
 	res := GetPresensiCurrentMonth(MongoConn)
-	fmt.Println(res)
 	filename := "rekapbulanini.csv"
 	file, err := os.Create(filename)
 	if err != nil {
-		log.Fatalln("failed to create file", err)
 		atmessage.SendMessage("failed to create file", to, whatsapp)
 	}
 	cw := csv.NewWriter(file)
-	cw.Write([]string{"DateTime", "Location", "Phone_Number", "CheckIn", "Nama", "Jabatan"})
+	err = cw.Write([]string{"DateTime", "Location", "Phone_Number", "CheckIn", "Nama", "Jabatan"})
+	if err != nil {
+		atmessage.SendMessage("failed to write file", to, whatsapp)
+	}
 	for _, prn := range res {
 		waktuutc := prn.Datetime.Time().In(location).String()
-		cw.Write([]string{waktuutc, prn.Location, prn.Phone_number, prn.Checkin, prn.Biodata.Nama, prn.Biodata.Jabatan})
+		err = cw.Write([]string{waktuutc, prn.Location, prn.Phone_number, prn.Checkin, prn.Biodata.Nama, prn.Biodata.Jabatan})
+		if err != nil {
+			atmessage.SendMessage("failed to write file", to, whatsapp)
+		}
+
 	}
 	cw.Flush()
 	filebyte, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatalln("failed to open file", err)
-		atmessage.SendMessage("failed to open file", to, whatsapp)
+		atmessage.SendMessage("failed to Read file", to, whatsapp)
 	}
 	resp, err = atmessage.SendDocumentMessage(filebyte, filename, filename, "application/csv", to, whatsapp)
 	return
